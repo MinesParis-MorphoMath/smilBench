bdba3e4f68883adb2da03323da02c3c272406a78
Jose-Marcio Martins da Cruz <Jose-Marcio.Martins@mines-paristech.fr>
2021-08-15 16:53:44 +0200

    Buglet
     On branch master
    
     Changes to be committed:
    	modified:   bin/mk-mosaic.py

--- bin/mk-mosaic.py
+++ bin/mk-mosaic.py
@@ -317,8 +317,9 @@ if __name__ == '__main__':
     # taurus-watershed-lena-skimage-04-01-16384.csv
     bName = os.path.basename(cli.files[0])
     iName, _ = os.path.splitext(bName)
-    fmt = "usage-{:s}-{:s}-{:s}-{:03d}-{:02d}-{!05d}.csv"
-    print(fmt.format(cli.function, iName, cli.which, cli.ri, cli.nr, cli.imsize)
+    fmt = "usage-{:s}-{:s}-{:s}-{:03d}-{:02d}-{:05d}.csv"
+    print(
+      fmt.format(cli.function, iName, cli.which, cli.ri, cli.nr, cli.imsize))
     pid = os.getpid()
     print("pid : {:d}".format(pid))
     input("Hit enter to continue")
